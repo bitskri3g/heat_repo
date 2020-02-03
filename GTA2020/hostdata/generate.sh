a04401382d48b4be913e7e8b2b7f8553c8f8af16
Chris Apsey <bitskrieg@bitskrieg.net>
2020-02-03 14:25:49 -0500

    Add sample generation file

---
+++ GTA2020/hostdata/generate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt update
+apt install tcpreplay tcpdump wget
+
+#test w single pcap
+wget https://download.netresec.com/pcap/ists-12/2015-03-07/snort.log.1425741002 -O 1.pcap
+
+for pcap in *.pcap
+do
+  tcpprep -i $pcap -o $pcap.cache -a bridge
+  tcprewrite --endpoints=1.2.3.4:4.3.2.1 --infile=$pcap --outfile=$pcap.addresses --cache=$pcap.cache
+  editcap -t 154000000 $pcap.addresses $pcap.addresses.time
+done
