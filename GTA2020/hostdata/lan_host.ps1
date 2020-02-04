02be107b2d08c9c552b39357b82195fafbee1627
bitskrieg <bitskrieg@bitskrieg.net>
2020-02-03 21:08:24 -0500

    drop noisy.py deps in favor of central traffic generator

--- GTA2020/hostdata/lan_host.ps1
+++ GTA2020/hostdata/lan_host.ps1
@@ -29,7 +29,6 @@ do {
   }
 }
 While ($break -eq $false)
-exit 1003
 }
 
 net user /add administrator $password /y
@@ -41,14 +40,4 @@ Invoke-WebRequest -Uri 'https://github.com/ytisf/theZoo/raw/master/malwares/Bina
 Invoke-WebRequest -Uri 'https://github.com/ytisf/theZoo/raw/master/malwares/Binaries/Win32.KeyPass/Win32.KeyPass.zip' -Outfile 'c:\keypass.zip'
 Invoke-WebRequest -Uri 'https://packages.wazuh.com/3.x/windows/wazuh-agent-3.9.5-1.msi' -Outfile 'c:\wazuh.msi'
 start-process c:\wazuh.msi -ArgumentList 'ADDRESS="so_master_address" AUTHD_SERVER="so_master_address" /passive' -wait
-## install python3
-Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.7.0/python-3.7.0.exe' -Outfile 'c:\python-3.7.0.exe'
-cd c:\
-.\python-3.7.0.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
-## install requests library with pip
-cd 'c:\Program Files (x86)/Python37-32/Scripts'
-pip install requests
-## download python script and config file
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/GA-CyberWorkforceAcademy/metaTest/master/TrafficGen/noisy.py' -Outfile 'c:\noisy.py'
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/GA-CyberWorkforceAcademy/metaTest/master/TrafficGen/config.json' -Outfile 'c:\config.json'
 exit 1001
